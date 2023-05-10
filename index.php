7896b32363d8165e672e0fcbf5823c5a021861af
nina alexsandra <ninaalexsandra99@gmail.com>
2023-05-10 11:36:24 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ProfileApp.com</title>
+</head>
+
+<body style="background:black;">
+    <div style="color:rgb(252, 252, 252); padding: 30px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+        <h1 style="color: blue;">My Personal information</h1>
+        <h3>Nina Alexsandra</h3>
+        <h3>Email: ninaalexsandra99@gmail.com</h3>
+        <h3>Phone: +62-878-8574-5276</h3>
+        <img src="Fotoo.jpeg">
+    </div>
+</body>
+
+</html>
